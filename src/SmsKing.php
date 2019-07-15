2ccea4cd437a729b3e66e91234e451bcf775341e
Lin Pin Chueh <javck.zack@gmail.com>
2019-07-15 20:35:10 +0800

    將函式改為非靜態

--- src/SmsKing.php
+++ src/SmsKing.php
@@ -10,6 +10,7 @@ namespace Javck\SmsKing;
 
 class SmsKing
 {
+
 	public function __construct()
 	{
 
@@ -20,12 +21,12 @@ class SmsKing
 	*	$smbody 簡訊內容
 	*	$mobile 傳送的電話號碼
 	*/
-	public static function sendSMS($smbody,$mobile) {
-		$smgUrl = 'https://api.kotsms.com.tw/kotsmsapi-1.php?username='.env('SMG_USERNAME', 'null').'&dstaddr='.$mobile.'&smbody='.mb_convert_encoding($smbody,"BIG5");
-		if(env('SMG_APIKEY',null) != null){
-			$smgUrl = $smgUrl .'&apikey='.env('SMG_APIKEY','');
-		}else if(env('SMG_PASSWORD',null) != null){
-			$smgUrl = $smgUrl .'&password='.env('SMG_PASSWORD','');
+	public function sendSMS($smbody,$mobile) {
+		$smgUrl = 'https://api.kotsms.com.tw/kotsmsapi-1.php?username='.config('smsking.SmgUsername').'&dstaddr='.$mobile.'&smbody='.mb_convert_encoding($smbody,"BIG5");
+		if(config('smsking.SmgKey') != null){
+			$smgUrl = $smgUrl .'&apikey='.config('smsking.SmgKey');
+		}else if(config('smsking.SmgPassword') != null){
+			$smgUrl = $smgUrl .'&password='.config('smsking.SmgPassword');
 		}
 		$smgStatusCode = explode("=",file_get_contents($smgUrl));
 		return $smgStatusCode[1];	
@@ -35,7 +36,7 @@ class SmsKing
 	*	簡訊王錯誤對應號碼
 	* 	$smgStatusCode 簡訊王回傳的代碼，流水號
 	*/
-	public static function smgTransStatusCode ($smgStatusCode) {
+	public function smgTransStatusCode ($smgStatusCode) {
 		switch ($smgStatusCode) {
 			case -1:
 				return "CGI string error ，系統維護中或其他錯誤 ,帶入的參數異常,伺服器異常";  
@@ -79,7 +80,7 @@ class SmsKing
 	*	$kmsgid 傳送簡訊時的紀錄編號
 	*
 	*/
-	public static function searchSmgStatusUrl($kmsgid) {
+	public function searchSmgStatusUrl($kmsgid) {
 		$smgStatusUrl = 'https://api.kotsms.com.tw/msgstatus.php?username='.env('SMG_USERNAME','null').'&apikey='.env('SMG_APIKEY','null').'&kmsgid='.$kmsgid;
 		$searchCode = explode("=",file_get_contents($smgStatusUrl));
 		return $searchCode[1];
@@ -90,11 +91,17 @@ class SmsKing
 	*	
 	*/	
 
-	public static function searchPointsUrl() {
-		$getpointsUrl = 'https://api.kotsms.com.tw/memberpoint.php?username='.env('SMG_USERNAME','null').'&apikey='.env('SMG_APIKEY','null');
+	public function searchPointsUrl() {
+		$getpointsUrl = 'https://api.kotsms.com.tw/memberpoint.php?username='.config('smsking.SmgUsername');
+		if(config('smsking.SmgKey') != null){
+			$getpointsUrl = $getpointsUrl .'&apikey='.config('smsking.SmgKey');
+		}else if(config('smsking.SmgPassword') != null){
+			$getpointsUrl = $getpointsUrl .'&password='.config('smsking.SmgPassword');
+		}
 		$pointsNumber = file_get_contents($getpointsUrl);
 		return $pointsNumber;
 	}
+
 
 
 }
